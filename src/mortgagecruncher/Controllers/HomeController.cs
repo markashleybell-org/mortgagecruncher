8b356abee4f1385d199b8a09ae953dabbdb36881
Mark Bell <me@markb.co.uk>
2016-07-02 16:05:55 +0100

    Add date picker to start date field, sort out validation

--- src/mortgagecruncher/Controllers/HomeController.cs
+++ src/mortgagecruncher/Controllers/HomeController.cs
@@ -1,25 +1,47 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using mortgagecruncher.Models;
 using mortgagecruncher.ViewModels;
+using System;
+using System.Globalization;
 
 namespace mortgagecruncher.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index(IndexViewModel model)
+        public IActionResult Index()
         {
-            if(model.StartMonth > 0)
+            return View("Calculate", new IndexViewModel {
+                LoanValue = 100000,
+                TermYears = 25,
+                TermRate = 4.49M
+            });
+        }
+
+        public IActionResult Calculate(IndexViewModel model)
+        {
+            if(!ModelState.IsValid)
+                return View(model);
+
+            if(!string.IsNullOrWhiteSpace(model.StartDate))
             { 
+                DateTime startDate;
+
+                if(!DateTime.TryParseExact(model.StartDate, "dd/MM/yyyy", null, DateTimeStyles.None, out startDate))
+                {
+                    ModelState.AddModelError("StartDate", "Date must be in DD/MM/YYYY format.");
+                    return View(model);
+                }
+
                 var schedule = new AmortisationSchedule(
-                    model.StartMonth,
-                    model.StartYear,
+                    startDate.Month,
+                    startDate.Year,
                     model.LoanValue,
                     (model.TermYears * 12),
                     model.TermRate,
-                    (model.FixedTermYears * 12),
-                    model.FixedTermRate,
-                    model.ExtraPaymentInterval,
-                    model.ExtraPaymentAmount
+                    (model.FixedTermYears.HasValue ? model.FixedTermYears.Value * 12 : 0),
+                    model.FixedTermRate ?? 0,
+                    model.ExtraPaymentInterval ?? 0,
+                    model.ExtraPaymentAmount ?? 0
                 );
 
                 model.ScheduleEntries = schedule.ScheduleEntries;
--- src/mortgagecruncher/ViewModels/IndexViewModel.cs
+++ src/mortgagecruncher/ViewModels/IndexViewModel.cs
@@ -9,36 +9,32 @@ namespace mortgagecruncher.ViewModels
 {
     public class IndexViewModel
     {
-        [Display(Name = "Term Start Month")]
-        public int StartMonth { get; set; } 
-        [Display(Name = "Term Start Year")]
-        public int StartYear { get; set; } 
+        [Required]
+        [Display(Name = "Term Start Date")]
+        public string StartDate { get; set; } 
+        [Required(ErrorMessage = "NOPE")]
         [Display(Name = "Loan Value")]
         public decimal LoanValue { get; set; } 
+        [Required]
         [Display(Name = "Term Years")]
         public int TermYears { get; set; } 
         [Display(Name = "Term Rate")]
         public decimal TermRate { get; set; } 
         [Display(Name = "Fixed Term Years")]
-        public int FixedTermYears { get; set; } 
+        public int? FixedTermYears { get; set; } 
         [Display(Name = "Fixed Term Rate")]
-        public decimal FixedTermRate { get; set; } 
+        public decimal? FixedTermRate { get; set; } 
         [Display(Name = "Extra Payment Interval")]
-        public int ExtraPaymentInterval { get; set; } 
+        public int? ExtraPaymentInterval { get; set; } 
         [Display(Name = "Extra Payment Amount")]
-        public decimal ExtraPaymentAmount { get; set; } 
+        public decimal? ExtraPaymentAmount { get; set; } 
 
         public string[] MonthNames { get; private set; }
-
-        public IEnumerable<SelectListItem> MonthOptions { get; set; }
-        public IEnumerable<SelectListItem> YearOptions { get; set; }
 
         public IEnumerable<AmortisationScheduleEntry> ScheduleEntries { get; set; }
 
         public IndexViewModel()
         {
-            var now = DateTime.Now;
-
             MonthNames = new[] {
                 "January",
                 "February",
@@ -53,18 +49,6 @@ namespace mortgagecruncher.ViewModels
                 "November",
                 "December"
             };
-
-            MonthOptions = Enumerable.Range(1, 12)
-                               .Select(x => new SelectListItem {
-                                   Value = x.ToString(),
-                                   Text = MonthNames[x - 1]
-                               });
-            
-            YearOptions = Enumerable.Range(now.Year - 4, 9)
-                              .Select(x => new SelectListItem {
-                                  Value = x.ToString(),
-                                  Text = x.ToString()
-                              });
 
             ScheduleEntries = new List<AmortisationScheduleEntry>();
         }
