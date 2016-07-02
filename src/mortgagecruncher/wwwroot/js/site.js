4cd407eeb0618a7a2884ea034a3ed5edfa8739a7
Mark Bell <me@markb.co.uk>
2016-07-02 16:04:23 +0100

    Install bootstrap-datepicker package

--- src/mortgagecruncher/wwwroot/js/site.js
+++ src/mortgagecruncher/wwwroot/js/site.js
@@ -1,1 +1,7 @@
-﻿// Write your Javascript code.
+﻿$('.datepicker').datepicker({
+    format: 'dd/mm/yyyy',
+    autoclose: true,
+    weekStart: 1
+}).on('changeDate', function(e) {
+    // console.log(e);
+});
