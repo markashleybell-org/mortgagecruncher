60d186602bb2213eff1ce13e8402373052947007
Mark Bell <me@markb.co.uk>
2016-07-04 07:16:44 +0100

    Add scaffold for popover help

--- src/mortgagecruncher/wwwroot/js/site.js
+++ src/mortgagecruncher/wwwroot/js/site.js
@@ -1,7 +1,11 @@
-﻿$('.datepicker').datepicker({
-    format: 'dd/mm/yyyy',
-    autoclose: true,
-    weekStart: 1
-}).on('changeDate', function(e) {
-    // console.log(e);
+﻿$(function() {
+    $('.datepicker').datepicker({
+        format: 'dd/mm/yyyy',
+        autoclose: true,
+        weekStart: 1
+    }).on('changeDate', function(e) {
+        // console.log(e);
+    });
+
+    $('#StartDate').popover({ trigger: 'focus', title: 'TEST', content: 'This is a test' });
 });
