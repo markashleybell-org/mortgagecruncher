e315647bcb5fc17a11e1b1973f44ce60af65bb63
Mark Bell <me@markb.co.uk>
2017-11-06 13:24:38 +0000

    Implement cookie persistence for settings

--- web/Controllers/HomeController.cs
+++ web/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using core;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Globalization;
@@ -11,19 +12,32 @@ namespace web.Controllers
 {
     public class HomeController : Controller
     {
+        public const string _cookieName = "mc_settings";
+
         public ActionResult Index()
         {
-            return View(nameof(Calculate), new IndexViewModel {
+            var model = new IndexViewModel {
                 StartDate = new DateTime(2015, 7, 25).ToString("dd/MM/yyyy"), // DateTime.Now.ToString("dd/MM/yyyy"),
                 LoanValue = 116250,
                 TermYears = 25,
                 TermRate = 4.49,
                 FixedTermYears = 5,
                 FixedTermRate = 2.95
-            });
+            };
+
+            var cookie = HttpContext.Request.Cookies[_cookieName];
+
+            if (cookie != null)
+            {
+                model = JsonConvert.DeserializeObject<IndexViewModel>(cookie.Value);
+                model.ScheduleEntries = GetSchedule(model);
+            }
+
+            return View(model);
         }
 
-        public ActionResult Calculate(IndexViewModel model)
+        [HttpPost]
+        public ActionResult Index(IndexViewModel model)
         {
             if (!ModelState.IsValid)
                 return View(model);
@@ -33,43 +47,56 @@ namespace web.Controllers
             if (!string.IsNullOrWhiteSpace(model.StartDate))
             {
                 if (!DateTime.TryParseExact(model.StartDate, "dd/MM/yyyy", null, DateTimeStyles.None, out var startDate))
-                    ModelState.AddModelError("StartDate", "Date must be in DD/MM/YYYY format.");
+                    ModelState.AddModelError("StartDate", "Start date must be in DD/MM/YYYY format.");
 
                 if (!ModelState.IsValid)
                     return View(model);
+                
+                var cookie = HttpContext.Request.Cookies[_cookieName] ?? new HttpCookie(_cookieName);
+                cookie.HttpOnly = true;
+                cookie.Secure = true;
+                cookie.Value = JsonConvert.SerializeObject(model);
+                cookie.Expires = DateTime.Now.AddDays(365);
+                HttpContext.Response.Cookies.Add(cookie);
 
-                var scheduleType = model.AmortisationScheduleType == AmortisationScheduleTypeViewModel.FixedPayments
-                                 ? ScheduleType.FixedPayments
-                                 : ScheduleType.FixedTerm;
-
-                var overPayments = model.OverPayments.Select((amt, idx) => new { idx = (double)(idx + 1), amt = (amt * -1) }).ToDictionary(x => x.idx, x => x.amt);
-
-                var schedule = AmortisationSchedule.create(
-                    model.TermYears,
-                    model.LoanValue,
-                    scheduleType,
-                    model.TermRate,
-                    (model.FixedTermYears.HasValue ? model.FixedTermYears.Value : 0),
-                    model.FixedTermRate ?? 0,
-                    overPayments
-                ).Select((row, i) => {
-                    var (per, rate, pmt, ipmt, ppmt, bal) = row;
-                    return new AmortisationScheduleEntryViewModel(
-                        paymentNumber: per,
-                        paymentDate: startDate.AddMonths(i),
-                        payment: Math.Abs(pmt),
-                        principal: Math.Abs(ppmt),
-                        interest: Math.Abs(ipmt),
-                        interestRateType: rate.Type,
-                        interestRate: rate.APR,
-                        balance: bal
-                    );
-                });
-
-                model.ScheduleEntries = schedule;
+                model.ScheduleEntries = GetSchedule(model);
             }
 
             return View(model);
+        }
+
+        private IEnumerable<AmortisationScheduleEntryViewModel> GetSchedule(IndexViewModel model)
+        {
+            if (!DateTime.TryParseExact(model.StartDate, "dd/MM/yyyy", null, DateTimeStyles.None, out var startDate))
+                throw new Exception("Start date must be in DD/MM/YYYY format.");
+
+            var scheduleType = model.AmortisationScheduleType == AmortisationScheduleTypeViewModel.FixedPayments
+                             ? ScheduleType.FixedPayments
+                             : ScheduleType.FixedTerm;
+
+            var overPayments = model.OverPayments.Select((amt, idx) => new { idx = (double)(idx + 1), amt = (amt * -1) }).ToDictionary(x => x.idx, x => x.amt);
+
+            return AmortisationSchedule.create(
+                model.TermYears,
+                model.LoanValue,
+                scheduleType,
+                model.TermRate,
+                (model.FixedTermYears.HasValue ? model.FixedTermYears.Value : 0),
+                model.FixedTermRate ?? 0,
+                overPayments
+            ).Select((row, i) => {
+                var (per, rate, pmt, ipmt, ppmt, bal) = row;
+                return new AmortisationScheduleEntryViewModel(
+                    paymentNumber: per,
+                    paymentDate: startDate.AddMonths(i),
+                    payment: Math.Abs(pmt),
+                    principal: Math.Abs(ppmt),
+                    interest: Math.Abs(ipmt),
+                    interestRateType: rate.Type,
+                    interestRate: rate.APR,
+                    balance: bal
+                );
+            });
         }
     }
 }
